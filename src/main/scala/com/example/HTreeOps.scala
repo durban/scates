aa82fc5a6fd3231d70cd9976f0be7c9f6eca12ed
Daniel Urban <daniel.urban@nokia.com>
2018-04-12 15:54:58 +0200

    More HTreeOps

--- src/main/scala/com/example/HTreeOps.scala
+++ src/main/scala/com/example/HTreeOps.scala
@@ -18,12 +18,13 @@ package com.example
 
 final object HTreeOps {
 
-  sealed trait Filter[T <: HTree, R[_], L <: St.Label[R]] {
+  /** Filters the steps of one label from the tree */
+  sealed abstract class Filter[T <: HTree, R[_], L <: St.Label[R]] {
     type Out <: HTree
     type Rest <: HTree
   }
 
-  object Filter extends Filter1 {
+  final object Filter extends Filter1 {
 
     type Aux[T <: HTree, R[_], L <: St.Label[R], O <: HTree, RST <: HTree] = Filter[T, R, L] {
       type Out = O
@@ -65,5 +66,114 @@ final object HTreeOps {
     implicit def matConsSkip[T <: HTree, R[_], L <: St.Label[R], H](
       implicit tl: Filter[T, R, L]
     ): Filter.Aux[H :: T, R, L, tl.Out, H :: tl.Rest] = mk
+  }
+
+  /** Witnesses that the resource is used consistently */
+  sealed abstract class ConsistentResource[T <: HTree, R[_], L <: St.Label[R] { type Lb = L }]
+
+  final object ConsistentResource {
+
+    def apply[T <: HTree, R[_], L <: St.Label[R] { type Lb = L }](implicit ev: ConsistentResource[T, R, L]): ConsistentResource[T, R, L] =
+      ev
+
+    implicit def mat[T <: HTree, O <: HTree, R[_], L <: St.Label[R] { type Lb = L }](
+      implicit
+      @evidence dae: DestroyedAtEnd.Aux[T, R, L, O],
+      @evidence tcs: TakesCorrectSteps[O, R, L]
+    ): ConsistentResource[T, R, L] = new ConsistentResource[T, R, L] {}
+  }
+
+  /** Witnesses that the resource is destroyed after usage */
+  sealed abstract class DestroyedAtEnd[T <: HTree, R[_], L <: St.Label[R]] {
+    type Out <: HTree
+  }
+
+  final object DestroyedAtEnd extends DestroyedAtEnd1 {
+
+    type Aux[T <: HTree, R[_], L <: St.Label[R], O <: HTree] = DestroyedAtEnd[T, R, L] {
+      type Out = O
+    }
+
+    def apply[T <: HTree, R[_], L <: St.Label[R]](implicit ev: DestroyedAtEnd[T, R, L]): DestroyedAtEnd.Aux[T, R, L, ev.Out] =
+      ev
+
+    private[HTreeOps] def mk[T <: HTree, R[_], L <: St.Label[R], O <: HTree]: DestroyedAtEnd.Aux[T, R, L, O] =
+      new DestroyedAtEnd[T, R, L] { type Out = O }
+
+    implicit def matSingle[T <: HTree, R[_], L <: St.Label[R]]: DestroyedAtEnd.Aux[St.Delete[R, L] :: T, R, L, T] =
+      mk
+  }
+
+  private[HTreeOps] sealed abstract class DestroyedAtEnd1 { this: DestroyedAtEnd.type =>
+    implicit def matFork[LHS <: HTree, RHS <: HTree, R[_], L <: St.Label[R]]: DestroyedAtEnd.Aux[(St.Delete[R, L] :: LHS) :+: (St.Delete[R, L] :: RHS), R, L, LHS :+: RHS] =
+      mk
+  }
+
+  /**
+   * Witnesses that during usage, there are only correct steps/transitions.
+   *
+   * Correct steps are:<br>
+   * - move then move (between allowed states)<br>
+   * - move then read (the correct state)<br>
+   * - read then move (to allowed state)<br>
+   * - read then read (the same state)<br>
+   * - move to init state (at the beginning)<br>
+   * - forks<br>
+   * - having exactly 0 steps is also correct
+   */
+  sealed abstract class TakesCorrectSteps[T <: HTree, R[_], L <: St.Label[R]]
+
+  final object TakesCorrectSteps extends TakesCorrectSteps1 {
+
+    def apply[T <: HTree, R[_], L <: St.Label[R]](implicit ev: TakesCorrectSteps[T, R, L]): TakesCorrectSteps[T, R, L] =
+      ev
+
+    private[HTreeOps] def mk[T <: HTree, R[_], L <: St.Label[R]]: TakesCorrectSteps[T, R, L] =
+      new TakesCorrectSteps[T, R, L] {}
+
+    implicit def moveMove[T <: HTree, S1, S2, R[_], L <: St.Label[R]](
+      implicit
+      @evidence transition: St.Api.Transition[R, S1, S2],
+      @evidence tail: TakesCorrectSteps[St.Move[R, S1, L] :: T, R, L]
+    ): TakesCorrectSteps[St.Move[R, S2, L] :: St.Move[R, S1, L] :: T, R, L] = mk
+  }
+
+  private[HTreeOps] sealed abstract class TakesCorrectSteps1 extends TakesCorrectSteps2 { this: TakesCorrectSteps.type =>
+    implicit def moveRead[T <: HTree, S, R[_], L <: St.Label[R]](
+      implicit @evidence tail: TakesCorrectSteps[St.Move[R, S, L] :: T, R, L]
+    ): TakesCorrectSteps[St.InState[R, S, L] :: St.Move[R, S, L] :: T, R, L] = mk
+  }
+
+  private[HTreeOps] sealed abstract class TakesCorrectSteps2 extends TakesCorrectSteps3 { this: TakesCorrectSteps.type =>
+    implicit def readMove[T <: HTree, S1, S2, R[_], L <: St.Label[R]](
+      implicit
+      @evidence transition: St.Api.Transition[R, S1, S2],
+      @evidence tail: TakesCorrectSteps[St.InState[R, S1, L] :: T, R, L]
+    ): TakesCorrectSteps[St.Move[R, S2, L] :: St.InState[R, S1, L] :: T, R, L] = mk
+  }
+
+  private[HTreeOps] sealed abstract class TakesCorrectSteps3 extends TakesCorrectSteps4 { this: TakesCorrectSteps.type =>
+    implicit def readRead[T <: HTree, S, R[_], L <: St.Label[R]](
+      implicit @evidence tail: TakesCorrectSteps[St.InState[R, S, L] :: T, R, L]
+    ): TakesCorrectSteps[St.InState[R, S, L] :: St.InState[R, S, L] :: T, R, L] = mk
+  }
+
+  private[HTreeOps] sealed abstract class TakesCorrectSteps4 extends TakesCorrectSteps5 { this: TakesCorrectSteps.type =>
+    implicit def moveToInit[T <: HTree, S, R[_], L <: St.Label[R]](
+      implicit @evidence init: St.Api.Initial[R, S]
+    ): TakesCorrectSteps[St.Move[R, S, L] :: HNil, R, L] = mk
+  }
+
+  private[HTreeOps] sealed abstract class TakesCorrectSteps5 extends TakesCorrectSteps6 { this: TakesCorrectSteps.type =>
+    implicit def fork[LHS <: HTree, RHS <: HTree, R[_], L <: St.Label[R]](
+      implicit
+      @evidence lhs: TakesCorrectSteps[LHS, R, L],
+      @evidence rhs: TakesCorrectSteps[RHS, R, L]
+    ): TakesCorrectSteps[LHS :+: RHS, R, L] = mk
+  }
+
+  private[HTreeOps] sealed abstract class TakesCorrectSteps6 { this: TakesCorrectSteps.type =>
+    implicit def end[R[_], L <: St.Label[R]]: TakesCorrectSteps[HNil, R, L] =
+      mk
   }
 }
--- src/test/scala/com/example/HTreeOpsSpec.scala
+++ src/test/scala/com/example/HTreeOpsSpec.scala
@@ -16,114 +16,185 @@
 
 package com.example
 
+import shapeless.test.illTyped
+
 import HTreeOps._
 
 final object HTreeOpsSpec {
 
   final class Rs1[A]
+
+  final object Rs1 {
+
+    implicit def initInt: St.Api.Initial[Rs1, Int] =
+      new St.Api.Initial[Rs1, Int] {}
+
+    implicit def intToByte: St.Api.Transition[Rs1, Int, Byte] =
+      new St.Api.Transition[Rs1, Int, Byte] {}
+  }
+
   final class Rs2[A]
 
-  val l: St.Label[Rs1] = null
-  val m: St.Label[Rs2] = null
+  val l: St.Label[Rs1] { type Lb = this.type } = null
+  val m: St.Label[Rs2] { type Lb = this.type } = null
 
-  val f1 = Filter[HNil, Rs1, l.Lb]
-  implicitly[f1.Out =:= HNil]
-  implicitly[f1.Rest =:= HNil]
+  final object filter {
 
-  val f2 = Filter[St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
-  implicitly[f2.Out =:= (St.Move[Rs1, Int, l.Lb] :: HNil)]
-  implicitly[f2.Rest =:= HNil]
+    val f1 = Filter[HNil, Rs1, l.Lb]
+    implicitly[f1.Out =:= HNil]
+    implicitly[f1.Rest =:= HNil]
 
-  val f3 = Filter[St.Move[Rs2, Int, m.Lb] :: HNil, Rs1, l.Lb]
-  implicitly[f3.Out =:= HNil]
-  implicitly[f3.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
+    val f2 = Filter[St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[f2.Out =:= (St.Move[Rs1, Int, l.Lb] :: HNil)]
+    implicitly[f2.Rest =:= HNil]
 
-  val f4 = Filter[St.Move[Rs2, Int, m.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
-  implicitly[f4.Out =:= (St.Move[Rs1, Int, l.Lb] :: HNil)]
-  implicitly[f4.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
+    val f3 = Filter[St.Move[Rs2, Int, m.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[f3.Out =:= HNil]
+    implicitly[f3.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
 
-  val f5 = Filter[St.Move[Rs1, Float, l.Lb] :: St.Move[Rs2, Int, m.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
-  implicitly[f5.Out =:= (St.Move[Rs1, Float, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil)]
-  implicitly[f5.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
+    val f4 = Filter[St.Move[Rs2, Int, m.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[f4.Out =:= (St.Move[Rs1, Int, l.Lb] :: HNil)]
+    implicitly[f4.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
 
-  val f6 = Filter[St.InState[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
-  implicitly[f6.Out =:= (St.InState[Rs1, Int, l.Lb] :: HNil)]
-  implicitly[f6.Rest =:= HNil]
+    val f5 = Filter[St.Move[Rs1, Float, l.Lb] :: St.Move[Rs2, Int, m.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[f5.Out =:= (St.Move[Rs1, Float, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil)]
+    implicitly[f5.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
 
-  val f7 = Filter[St.InState[Rs2, Int, m.Lb] :: HNil, Rs1, l.Lb]
-  implicitly[f7.Out =:= HNil]
-  implicitly[f7.Rest =:= (St.InState[Rs2, Int, m.Lb] :: HNil)]
+    val f6 = Filter[St.InState[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[f6.Out =:= (St.InState[Rs1, Int, l.Lb] :: HNil)]
+    implicitly[f6.Rest =:= HNil]
 
-  val f8 = Filter[St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
-  implicitly[f8.Out =:= (St.InState[Rs1, Int, l.Lb] :: HNil)]
-  implicitly[f8.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
+    val f7 = Filter[St.InState[Rs2, Int, m.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[f7.Out =:= HNil]
+    implicitly[f7.Rest =:= (St.InState[Rs2, Int, m.Lb] :: HNil)]
 
-  val f9 = Filter[St.Move[Rs1, Float, l.Lb] :: St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
-  implicitly[f9.Out =:= (St.Move[Rs1, Float, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil)]
-  implicitly[f9.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
+    val f8 = Filter[St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[f8.Out =:= (St.InState[Rs1, Int, l.Lb] :: HNil)]
+    implicitly[f8.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
 
-  val f10 = Filter[HNil :+: HNil, Rs1, l.Lb]
-  implicitly[f10.Out =:= (HNil :+: HNil)]
-  implicitly[f10.Rest =:= (HNil :+: HNil)]
+    val f9 = Filter[St.Move[Rs1, Float, l.Lb] :: St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[f9.Out =:= (St.Move[Rs1, Float, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil)]
+    implicitly[f9.Rest =:= (St.Move[Rs2, Int, m.Lb] :: HNil)]
 
-  val f11 = Filter[(St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil, Rs1, l.Lb]
-  implicitly[f11.Out =:= ((St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil)]
-  implicitly[f11.Rest =:= (HNil :+: HNil)]
+    val f10 = Filter[HNil :+: HNil, Rs1, l.Lb]
+    implicitly[f10.Out =:= (HNil :+: HNil)]
+    implicitly[f10.Rest =:= (HNil :+: HNil)]
 
-  val f12 = Filter[(St.Move[Rs2, Int, m.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil, Rs1, l.Lb]
-  implicitly[f12.Out =:= ((St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil)]
-  implicitly[f12.Rest =:= ((St.Move[Rs2, Int, m.Lb] :: HNil) :+: HNil)]
+    val f11 = Filter[(St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil, Rs1, l.Lb]
+    implicitly[f11.Out =:= ((St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil)]
+    implicitly[f11.Rest =:= (HNil :+: HNil)]
 
-  val f13 = Filter[(St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil) :+: HNil, Rs1, l.Lb]
-  implicitly[f13.Out =:= ((St.InState[Rs1, Int, l.Lb] :: HNil) :+: HNil)]
-  implicitly[f13.Rest =:= ((St.Move[Rs2, Int, m.Lb] :: HNil) :+: HNil)]
+    val f12 = Filter[(St.Move[Rs2, Int, m.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil, Rs1, l.Lb]
+    implicitly[f12.Out =:= ((St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil)]
+    implicitly[f12.Rest =:= ((St.Move[Rs2, Int, m.Lb] :: HNil) :+: HNil)]
 
-  val f14 = Filter[(St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil) :+: (St.Move[Rs1, Float, l.Lb] :: HNil), Rs1, l.Lb]
-  implicitly[f14.Out =:= ((St.InState[Rs1, Int, l.Lb] :: HNil) :+: (St.Move[Rs1, Float, l.Lb] :: HNil))]
-  implicitly[f14.Rest =:= ((St.Move[Rs2, Int, m.Lb] :: HNil) :+: HNil)]
+    val f13 = Filter[(St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil) :+: HNil, Rs1, l.Lb]
+    implicitly[f13.Out =:= ((St.InState[Rs1, Int, l.Lb] :: HNil) :+: HNil)]
+    implicitly[f13.Rest =:= ((St.Move[Rs2, Int, m.Lb] :: HNil) :+: HNil)]
 
-  val f15 = Filter[(St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil) :+: (St.InState[Rs2, Boolean, m.Lb] :: St.Move[Rs1, Float, l.Lb] :: HNil), Rs1, l.Lb]
-  implicitly[f15.Out =:= ((St.InState[Rs1, Int, l.Lb] :: HNil) :+: (St.Move[Rs1, Float, l.Lb] :: HNil))]
-  implicitly[f15.Rest =:= ((St.Move[Rs2, Int, m.Lb] :: HNil) :+: (St.InState[Rs2, Boolean, m.Lb] :: HNil))]
+    val f14 = Filter[(St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil) :+: (St.Move[Rs1, Float, l.Lb] :: HNil), Rs1, l.Lb]
+    implicitly[f14.Out =:= ((St.InState[Rs1, Int, l.Lb] :: HNil) :+: (St.Move[Rs1, Float, l.Lb] :: HNil))]
+    implicitly[f14.Rest =:= ((St.Move[Rs2, Int, m.Lb] :: HNil) :+: HNil)]
 
-  val f16 = Filter[
-    (
+    val f15 = Filter[(St.Move[Rs2, Int, m.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil) :+: (St.InState[Rs2, Boolean, m.Lb] :: St.Move[Rs1, Float, l.Lb] :: HNil), Rs1, l.Lb]
+    implicitly[f15.Out =:= ((St.InState[Rs1, Int, l.Lb] :: HNil) :+: (St.Move[Rs1, Float, l.Lb] :: HNil))]
+    implicitly[f15.Rest =:= ((St.Move[Rs2, Int, m.Lb] :: HNil) :+: (St.InState[Rs2, Boolean, m.Lb] :: HNil))]
+
+    val f16 = Filter[
       (
-        (St.InState[Rs1, Float, l.Lb] :: St.Move[Rs1, Float, l.Lb] :: HNil) :+:
-        (St.Move[Rs1, Float, l.Lb] :: St.Move[Rs2, Double, m.Lb] :: HNil)
-      ) :+:
+        (
+          (St.InState[Rs1, Float, l.Lb] :: St.Move[Rs1, Float, l.Lb] :: HNil) :+:
+          (St.Move[Rs1, Float, l.Lb] :: St.Move[Rs2, Double, m.Lb] :: HNil)
+        ) :+:
+        (
+          (St.InState[Rs2, Float, m.Lb] :: St.Move[Rs2, Float, m.Lb] :: St.Move[Rs1, Float, l.Lb] :: HNil) :+:
+          (St.InState[Rs2, Float, m.Lb] :: HNil)
+        )
+      ),
+      Rs1,
+      l.Lb
+    ]
+    implicitly[
+      f16.Out =:=
       (
-        (St.InState[Rs2, Float, m.Lb] :: St.Move[Rs2, Float, m.Lb] :: St.Move[Rs1, Float, l.Lb] :: HNil) :+:
-        (St.InState[Rs2, Float, m.Lb] :: HNil)
+        (
+          (St.InState[Rs1, Float, l.Lb] :: St.Move[Rs1, Float, l.Lb] :: HNil) :+:
+          (St.Move[Rs1, Float, l.Lb] :: HNil)
+        ) :+:
+        (
+          (St.Move[Rs1, Float, l.Lb] :: HNil) :+:
+          (HNil)
+        )
       )
-    ),
-    Rs1,
-    l.Lb
-  ]
-  implicitly[
-    f16.Out =:=
-    (
+    ]
+    implicitly[
+      f16.Rest =:=
       (
-        (St.InState[Rs1, Float, l.Lb] :: St.Move[Rs1, Float, l.Lb] :: HNil) :+:
-        (St.Move[Rs1, Float, l.Lb] :: HNil)
-      ) :+:
-      (
-        (St.Move[Rs1, Float, l.Lb] :: HNil) :+:
-        (HNil)
+        (
+          (HNil) :+:
+          (St.Move[Rs2, Double, m.Lb] :: HNil)
+        ) :+:
+        (
+          (St.InState[Rs2, Float, m.Lb] :: St.Move[Rs2, Float, m.Lb] :: HNil) :+:
+          (St.InState[Rs2, Float, m.Lb] :: HNil)
+        )
       )
-    )
-  ]
-  implicitly[
-    f16.Rest =:=
-    (
-      (
-        (HNil) :+:
-        (St.Move[Rs2, Double, m.Lb] :: HNil)
-      ) :+:
-      (
-        (St.InState[Rs2, Float, m.Lb] :: St.Move[Rs2, Float, m.Lb] :: HNil) :+:
-        (St.InState[Rs2, Float, m.Lb] :: HNil)
-      )
-    )
-  ]
+    ]
+  }
+
+  final object destroyedAtEnd {
+
+    val d1 = DestroyedAtEnd[St.Delete[Rs1, l.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[d1.Out =:= HNil]
+
+    val d2 = DestroyedAtEnd[St.Delete[Rs1, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    implicitly[d2.Out =:= (St.Move[Rs1, Int, l.Lb] :: HNil)]
+
+    val d3 = DestroyedAtEnd[(St.Delete[Rs1, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil) :+: (St.Delete[Rs1, l.Lb] :: HNil), Rs1, l.Lb]
+    implicitly[d3.Out =:= ((St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil)]
+
+    illTyped("""DestroyedAtEnd[HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+    illTyped("""DestroyedAtEnd[St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+    illTyped("""DestroyedAtEnd[St.InState[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+    illTyped("""DestroyedAtEnd[(St.Move[Rs1, Int, l.Lb] :: HNil) :+: (St.Delete[Rs1, l.Lb] :: HNil), Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+    illTyped("""DestroyedAtEnd[((St.Delete[Rs1, l.Lb] :: HNil) :+: (St.Move[Rs1, Int, l.Lb] :: HNil)) :+: (St.Delete[Rs1, l.Lb] :: HNil), Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+  }
+
+  final object takesCorrectSteps {
+
+    TakesCorrectSteps[HNil, Rs1, l.Lb]
+    TakesCorrectSteps[St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    illTyped("""TakesCorrectSteps[St.Move[Rs1, Float, l.Lb] :: HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+    illTyped("""TakesCorrectSteps[St.InState[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+    illTyped("""TakesCorrectSteps[St.InState[Rs1, Byte, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+    illTyped("""TakesCorrectSteps[(HNil) :+: (St.InState[Rs1, Byte, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil), Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+
+    TakesCorrectSteps[St.InState[Rs1, Int, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    illTyped("""TakesCorrectSteps[St.InState[Rs1, Float, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+
+    TakesCorrectSteps[St.Move[Rs1, Byte, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    illTyped("""TakesCorrectSteps[St.Move[Rs1, Float, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+
+    TakesCorrectSteps[St.Move[Rs1, Byte, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    illTyped("""TakesCorrectSteps[St.Move[Rs1, Float, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+
+    TakesCorrectSteps[St.InState[Rs1, Int, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]
+    illTyped("""TakesCorrectSteps[St.InState[Rs1, Byte, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil, Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+
+    TakesCorrectSteps[HNil :+: HNil, Rs1, l.Lb]
+    TakesCorrectSteps[(St.Move[Rs1, Int, l.Lb] :: HNil) :+: HNil, Rs1, l.Lb]
+    TakesCorrectSteps[(St.Move[Rs1, Int, l.Lb] :: HNil) :+: (St.InState[Rs1, Int, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil), Rs1, l.Lb]
+    illTyped("""TakesCorrectSteps[HNil :+: (St.Move[Rs1, Float, l.Lb] :: HNil), Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+    illTyped("""TakesCorrectSteps[HNil :+: (St.InState[Rs1, Int, l.Lb] :: HNil), Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+    illTyped("""TakesCorrectSteps[(St.Move[Rs1, Int, l.Lb] :: HNil) :+: (St.InState[Rs1, Byte, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil), Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+
+    TakesCorrectSteps[(St.InState[Rs1, Int, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil) :+: (St.InState[Rs1, Int, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil), Rs1, l.Lb]
+    illTyped("""TakesCorrectSteps[(St.InState[Rs1, Float, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil) :+: (St.InState[Rs1, Int, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil), Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+  }
+
+  final object consistentResource {
+    ConsistentResource[St.Delete[Rs1, l.Lb] :: HNil, Rs1, l.Lb]
+    ConsistentResource[(St.Delete[Rs1, l.Lb] :: HNil) :+: (St.Delete[Rs1, l.Lb] :: St.Move[Rs1, Int, l.Lb] :: HNil), Rs1, l.Lb]
+    illTyped("""ConsistentResource[(St.Delete[Rs1, l.Lb] :: HNil) :+: (St.Delete[Rs1, l.Lb] :: St.InState[Rs1, Int, l.Lb] :: HNil), Rs1, l.Lb]""", "could not find implicit value for parameter.*")
+  }
 }
