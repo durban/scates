76dc1a5207d118684473e21dc58724806c16e73e
Daniel Urban <daniel.urban@nokia.com>
2019-02-25 15:30:13 +0100

    Fix license headers

--- src/main/scala/com/example/utils.scala
+++ src/main/scala/com/example/utils.scala
@@ -118,7 +118,7 @@ object IxMonad {
     }
   }
 
-  implicit def ixMonadFromMonad[S[_]](implicit S: Monad[S]): IxMonad[Fake[S]#λ] = new IxMonad[Fake[S]#λ] {
+  def ixMonadFromMonad[S[_]](implicit S: Monad[S]): IxMonad[Fake[S]#λ] = new IxMonad[Fake[S]#λ] {
 
     override def flatMap[F, T, U, A, B](fa: S[A])(f: A => S[B]): S[B] =
       S.flatMap(fa)(f)
